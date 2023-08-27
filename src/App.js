ba0bc38808b14626190c354c6159d392c39058cd
StanParaclete <you@example.com>
2023-08-27 18:10:28 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import MyRoutes from "./MyRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <MyRoutes />
+      <Home />
       <Footer />
     </div>
   );
